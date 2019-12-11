6020c9207c5b24f8db96c3398e42f9a8891dcc59
Matthew Herman <maherman@directs.com>
2019-12-11 16:36:45 -0600

    Shhhh...nothing to see here

--- 04/exercise1.lisp
+++ 04/exercise1.lisp
@@ -4,11 +4,28 @@
 (defun increasing (digits)
     (apply #'<= digits))
 
+;; Example - (1 2 2 3)
+;; ()
+;; ((1))
+;; ((2) (1))
+;; ((2 2) (1))
+;; ((3) (2 2) (1))
+;;
+;; acc = ((1)) digit = 1
+;; (list (cons 1 (car ((1)) )) (cadr ((1)) ))
+;; (list (cons 1 (1) )         nil)
+;; (list (1 1)                 nil)
+;; ((1 1) nil)
+;;
+;; acc = ((1)) digit = 2
+;; (cons (list 2) ((1)) )
+;; (cons (2)      ((1)) )
+;; ((2) (1))
 (defun split-sequences (digits)
     (reduce
         #'(lambda (acc digit)
             (if (equal (car (car acc)) digit)
-                (list (cons digit (car acc)) (cadr acc))
+                (cons (cons digit (car acc)) (cdr acc))
                 (cons (list digit) acc)))
         digits
         :initial-value '()))
@@ -17,15 +34,15 @@
     (not
         (equal nil (find-if (lambda (x) (funcall fn x)) items))))
 
-(defun includes-sequence (digits)
-    (any #'(lambda (x) (>= (length x) 2)) (split-sequences digits)))
+(defun includes-sequence-of-n (digits n)
+    (any #'(lambda (x) (>= (length x) n)) (split-sequences digits)))
 
-(defun test (digits)
-    (and (increasing digits) (includes-sequence digits)))
+(defun test (password)
+    (let ((digits (int-to-list password)))
+    (and (increasing digits) (includes-sequence-of-n digits 2))))
 
 (defun find-matching-passwords (begin end)
-    (loop for x from begin to end when (test (int-to-list x)) collect x))
+    (loop for password from begin to end when (test password) collect password))
 
-;; Expect 1790
 (defun run ()
     (length (find-matching-passwords 147981 691423)))
