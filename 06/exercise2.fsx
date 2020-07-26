a4469a00e44a8c15a77a16bc56b1e172bc49d532
Matthew Herman <mattherman11@gmail.com>
2020-07-26 18:58:03 -0500

    Type-specific graph module

--- 06/exercise2.fsx
+++ 06/exercise2.fsx
@@ -1,30 +1,39 @@
 open System.IO
 
+let toTuple (arr: 't[]) =
+    match arr with
+    | [| a; b |] -> Some (a, b)
+    | _ -> None
+
 let loadOrbits filename =
     File.ReadLines filename
-    |> Seq.map (fun s -> s.Split ')')
+    |> Seq.map (fun s -> s.Split ')' |> toTuple)
+    |> Seq.choose id
     |> Seq.toList
 
-type Graph = Map<string, Set<string>>
+module Graph =
+    type Graph = Map<string, Set<string>>
 
-let edges vertex (graph: Graph) =
-    match graph.TryFind vertex with
-    | Some edges -> edges
-    | _ -> Set.empty
+    let edges vertex (graph: Graph) =
+        match graph.TryFind vertex with
+        | Some edges -> edges
+        | _ -> Set.empty
 
-let addVertex vertex (graph: Graph) =
-    if not (graph.ContainsKey vertex) then
-        graph.Add (vertex, Set.empty)
-    else
+    let addVertex vertex (graph: Graph) =
+        if not (graph.ContainsKey vertex) then
+            graph.Add (vertex, Set.empty)
+        else
+            graph
+
+    let addEdge vertexA vertexB (graph: Graph) =
+        let vertexAEdges = graph |> edges vertexA |> Set.add vertexB
+        let vertexBEdges = graph |> edges vertexB |> Set.add vertexA
         graph
+        |> Map.add vertexA vertexAEdges
+        |> Map.add vertexB vertexBEdges 
 
-let addEdge vertexA vertexB (graph: Graph) =
-    let vertexAEdges = graph |> edges vertexA |> Set.add vertexB
-    let vertexBEdges = graph |> edges vertexB |> Set.add vertexA
-    graph
-    |> Map.add vertexA vertexAEdges
-    |> Map.add vertexB vertexBEdges 
+    let build edges =
+        List.fold (fun graph (vertexA, vertexB) -> addEdge vertexA vertexB graph) Map.empty edges
 
-let build edges =
-    List.fold (fun graph (vertexA, vertexB) -> addEdge vertexA vertexB graph) Map.empty edges
-
+let orbits = loadOrbits "simple-input2.txt"
+let graph = Graph.build orbits
